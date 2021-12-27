659e9f67a5c7f9862e4b9a43c33efa66805c533a
haesoo <wjdgotn77@gmail.com>
2021-12-27 20:35:45 +0900

    Add: 초기셋팅추가

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,5 @@
 import React from 'react';
 
 export default function Nav() {
-  return (
-    <div className="nav">
-      <p>ffdif;k;ldsk;lfkds</p>
-      {/* <span>아어마ㅣㅓㅏㅣ너</span> */}
-    </div>
-  );
+  return <div></div>;
 }
