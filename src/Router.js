80b36d4d7dec06b0bae2006eda66cc79df562ae9
haesoo <wjdgotn77@gmail.com>
2021-12-27 20:31:30 +0900

    Add: 초기셋팅추가

---
+++ src/Router.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import Main from './pages/Main/Main';
+import Login from './pages/Login/Login';
+import ProductDetail from './pages/ProductDetail/ProductDetail';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/main" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product-detail" element={<ProductDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Nav() {
+  return (
+    <div className="nav">
+      <p>ffdif;k;ldsk;lfkds</p>
+      {/* <span>아어마ㅣㅓㅏㅣ너</span> */}
+    </div>
+  );
+}
