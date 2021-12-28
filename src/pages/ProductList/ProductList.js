b485f64f0c43430d741eb32919f5fe3c40fc9aca
haesoo <wjdgotn77@gmail.com>
2021-12-29 01:25:48 +0900

    Add: ProductList 페이지 - 상품리스트 구현
    - ProductList.js
     1. 백엔드와 통신 전 목데이터를 이용해 json 파일을 fetch 받아오도록 하였습니다.
     2. useEffect를 이용해 해당 데이터를 state로 관리합니다.
     3. Product Component 에 props으로 해당 리스트를 넘겨주며 map을 사용해 데이터의 갯수만큼 Product Component를 그리도록 만들었습니다.
    
    - Product.js
     1. 넘겨받은 props를 바탕으로 상품이미지,상품명,상품가격을 표시합니다.
     2. 위시리스트를 추가 기능으로 구현할 시 필요할 수 있는 아이콘을 추가하였습니다.

---
+++ src/pages/ProductList/ProductList.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import Product from './components/Product';
+import FilterButton from './components/FilterButton';
+import './ProductList.scss';
+
+const FETCH_URL = 'http://localhost:3001/';
+
+function ProductList() {
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    fetch(`${FETCH_URL}/data/ProductListData/ProductListData.json`, {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => setProductList(data));
+  }, []);
+
+  return (
+    <div className="productListContent">
+      <div className="productListHeader">New Arrivals</div>
+      <div className="productContour"></div>
+      <div className="filterButtons">
+        <FilterButton standard="사이즈" />
+        <FilterButton standard="SORT BY" />
+      </div>
+      <div className="productList">
+        {productList.map(product => {
+          const { product_id, ...productInfo } = product;
+          return <Product key={product_id} {...productInfo} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default ProductList;
---
+++ src/pages/ProductList/components/Product.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FaRegHeart } from 'react-icons/fa';
+import './Product.scss';
+
+export default function Product(props) {
+  const { url, name, price } = props;
+
+  return (
+    <div className="product">
+      <div className="imageHolder">
+        <img alt={`${name}`} src={url} />
+      </div>
+      <div className="description">
+        <div className="description__name">{name}</div>
+        <div className="description__price">{price}</div>
+        <div className="description__color">black</div>
+        <div className="description__wishButton">
+          <div className="productFilter"></div>
+          <FaRegHeart></FaRegHeart>
+        </div>
+      </div>
+    </div>
+  );
+}
