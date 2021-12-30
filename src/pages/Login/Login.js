49398e3ca1ea55878047282d8c1bc1ba1a0c1477
jihye kim <zziruru@jihyeui-MacBookPro.local>
2021-12-30 15:55:50 +0900

    Add : Login page 기능 구현 중

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import react from 'react';
+// import React from 'react-dom';
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <main className="user-container">
+      {/* <section className="login-content">
+        <div className="login-view">
+          <h2 className="login-view-title">
+            <span>로그인</span>
+          </h2>
+        </div>
+        // Login input//
+        <div className="loginContent">
+          <input
+            id="id"
+            type="text"
+            placeholder="이메일"
+            onChange={handleLoginInput}
+          />
+          <input
+            id="pw"
+            type="text"
+            placeholder="비밀번호"
+            onChange={setpassword}
+          />
+        </div>
+        <a href="">비밀번호를 잊으셨습니까?</a>
+        // Login BTN//
+        <div className="login-button-area">
+          <button type="button">
+            <span>로그인</span>
+          </button>
+        </div>
+        signup input
+        <section className="login-signup">
+          <h2 class="login-signup-title">
+            <span>등록</span>
+          </h2>
+          <p class="login-signup-text">
+            <span>
+              Zara.com 회원으로 가입하시면 빠르고 편리하게 이용하실 수 있습니다.{' '}
+            </span>
+          </p>
+          <div className="login-signup-text">
+            <span>
+              {' '}
+              "아직"
+              <strong> Zara.com </strong>
+              "의 회원이 아니시라면 이메일로 간편하게 가입하실 수 있습니다. "
+            </span>
+          </div>
+          // signup btn//
+          <div className="signup-button-area">
+            <button type="button">
+              <span>계정 만들기</span>
+            </button>
+          </div>
+        </section>
+      </section> */}
+    </main>
+  );
 }
 
 export default Login;
---
+++ src/pages/Login/Signup.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { BsExclamationCircle } from '';
+
+
+
+function signup () {
+  return (  
+  <main className='user-info-container'>
+    1
+  {/* <section className='user-info-content'>
+
+ // sign-up //     
+  <div className='main-class-header'>
+   <h1 class='main-class-header'>
+       <span>개인 정보</span>
+   </h1>
+  </div>
+  <form className= 'personal-info' id='adress form' autocomplete='off' method='POST' /> 
+   <div className= 'form_column' data-name='email' > 
+      <div className='form-email-input-label' placeholder='' type='text'>
+        <input className='form-email-input'> </input>
+        <label className='form-email-input' for='' id=' ' > 이메일 </label>
+      </div>
+      <div className= 'form-email-input-error'>
+       <span className='email-title'>이메일</span>
+       <svg className='email-title-info'> </svg>
+       <path></path>
+       <span> 필수 입력란입니다. </span>
+      </div>
+   </div>
+   <div className= 'form_column' form_column--empty> </div>
+
+      <div className= 'form_column' data-name='password' > </div>
+      <div className='form-pw-input-label' placeholder='' type='text'>
+        <input className='form-pw-input'> </input>
+        <label className='form-pw-input' for='' id=' ' > 비밀번호 </label>
+      </div>
+      <div className= 'form-pw-input-error'>
+       <span className='email-title'>비밀번호</span>
+       <svg className='email-title-info'> </svg>
+       <path></path>
+       <span> 필수 입력란입니다. </span>
+      </div>
+
+   <div className= 'form_column' data-name='password-confirm' > </div>
+   <div className='form-email-input-label' placeholder='' type='text'>
+        <input className='form-email-input'> </input>
+        <label className='form-email-input' for='' id=' ' > 비밀번호를 한 번 더 입력해 주십시오. </label>
+      </div>
+      <div className= 'form-email-input-error'>
+       <span className='email-title'>비밀번호를 한 번 더 입력해 주십시오.</span>
+       <svg className='email-title-info'> </svg>
+       <path></path>
+       <span> 필수 입력란입니다. </span>
+      </div>
+   <div className= 'form_column' form_column--empty> </div>
+
+   <div className= 'form_column' data-name='name'> </div>
+   <div className='form-email-input-label' placeholder='' type='text'>
+        <input className='form-email-input'> </input>
+        <label className='form-email-input' for='' id=' ' > 이름 </label>
+      </div>
+      <div className= 'form-email-input-error'>
+       <span className='email-title'>이름</span>
+       <svg className='email-title-info'> </svg>
+       <path></path>
+       <span> 필수 입력란입니다. </span>
+      </div>
+
+ // sign-up-checkbox //    
+    <label> 
+       <div className='form_column' data-name='checkAll'>
+       <div className='form-input'></div>
+       <div className='form-input-checkbox'></div>
+    <span></span>
+
+     
+// signup btn //
+     <div className='signup-button-area'>
+	   <button type='button'>
+		 <span>계정 만들기</span>
+	   </button>
+     </div>
+     
+  </>
+
+  <BsExclamationCircle/>
+
+
+  </section> */}
+  </main>
+  )
+  export default Signup;
