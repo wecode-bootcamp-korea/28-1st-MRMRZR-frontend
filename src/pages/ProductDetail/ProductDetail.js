9eefdb702aec797b2736896b41af681973c383e0
hyunjea Ann <hyunjeaann@hyunjeaui-MacBookAir.local>
2022-01-05 18:48:09 +0900

    WIP : 네브바 장바구니 작업 전 백업

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -8,33 +8,50 @@ function ProductDetail() {
   const [productVal, setProductVal] = useState({});
   const params = useParams();
 
-  // setProductVal({
-  //   id: 1,
-  //   name: '목업데이터 네임',
-  //   product_number: '목업데이터 제품번호',
-  //   description: '목업데이터 디스크립션',
-  //   price: '목업데이터 가격',
-  //   is_new: false,
-  //   image_urls: [
-  //     'https://cdn.pixabay.com/photo/2017/08/06/12/04-people-2591867_1280.jpg',
-  //   ],
-  //   sizes: [
-  //     { size_id: 1, size_name: 'XS' },
-  //     { size_id: 2, size_name: 'S' },
-  //     { size_id: 3, size_name: 'M' },
-  //     { size_id: 4, size_name: 'L' },
-  //     { size_id: 5, size_name: 'XL' },
-  //   ],
-  // });
-
   useEffect(() => {
-    // fetch(`http://10.58.7.205:8000/products/detail/1`)
-    fetch(`http://10.58.5.4:8000/products/detail/${params.id}`)
-      .then(res => res.json())
-      .then(res => {
-        setProductVal(res.results);
-      });
+    setProductVal({
+      id: 1,
+      name: '스웨트셔츠',
+      product_number: 'S00001',
+      description: '두꺼운 재질',
+      price: 99000,
+      is_new: true,
+      image_urls: [
+        {
+          image_id: 11,
+          image_url:
+            'https://cdn.pixabay.com/photo/2017/08/06/12/04/people-2591867_1280.jpg',
+        },
+        {
+          image_id: 12,
+          image_url:
+            'https://cdn.pixabay.com/photo/2017/08/06/12/04/people-2591867_1280.jpg',
+        },
+        {
+          image_id: 13,
+          image_url:
+            'https://cdn.pixabay.com/photo/2017/08/06/12/04/people-2591867_1280.jpg',
+        },
+      ],
+      sizes: [
+        { size_id: 1, size_name: 'XS', size_stock: 0 },
+        { size_id: 2, size_name: 'S', size_stock: 30 },
+        { size_id: 3, size_name: 'M', size_stock: 35 },
+        { size_id: 4, size_name: 'L', size_stock: 30 },
+        { size_id: 5, size_name: 'XL', size_stock: 20 },
+      ],
+    });
   }, []);
+
+  // useEffect(() => {
+  // fetch(`http://10.58.7.205:8000/products/detail/1`)
+  // fetch(`http://10.58.5.4:8000/products/detail/${params.id}`)
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       setProductVal(res.results);
+  //       console.log(productVal);
+  //     });
+  // }, []);
 
   return (
     <div className="productDetail">
